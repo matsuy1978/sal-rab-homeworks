c6cbc766eb74b039f761b3156e53fb643e9bb015
mikhail matsuy <miguelle1978@gmail.com>
2022-04-14 16:51:13 +0300

    task_3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,7 +2,7 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
+    product = {id: 1978, title: "корзина", count: 10, price: '100'};
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
@@ -14,14 +14,23 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
+    let itemCountText = '5*500 = 2500 ₽';
     // 1. Создайте переменную itemCountText, равную пустой строке
+    itemCountText = "";
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    itemCountText = itemCountText + count;
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    itemCountText = itemCountText + `x`;
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    itemCountText = itemCountText + price; 
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    itemCountText = itemCountText + ` ₽ = `;
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    let sum = count * price;
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    itemCountText = itemCountText + sum;
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    itemCountText = itemCountText + ` ₽`;
 
     // Конец решения задания №3.1.
 
