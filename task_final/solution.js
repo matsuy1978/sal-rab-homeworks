54694cc91661bd23013caf801ae3555a80c40c74
mikhail matsuy <miguelle1978@gmail.com>
2022-04-18 16:44:44 +0300

    task_finak

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,25 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, address, goods, sum)
 
+{
+    let data = {client: {name, phone}, order: {address, sum}, goods: []};
+    
+    
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i <= countOfGoods; i += 1 ) {
+        data.goods.title = data.goods.push(goods[i].title) 
+        data.goods.count = data.goods.push(goods[i].count)
     }
 
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
 
-    data.client = 'Иван';
+    data.client = 'Иван ';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
